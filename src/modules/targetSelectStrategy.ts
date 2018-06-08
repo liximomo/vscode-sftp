a84a1dba0ff47b2874cc3409ec6c53207ca5db23
Gergő Koós <g.koos@2mcgroup.com>
2018-06-08 08:43:03 +0100

    Fix for the Diff shortcut too

--- src/modules/targetSelectStrategy.ts
+++ src/modules/targetSelectStrategy.ts
@@ -117,7 +117,7 @@ export function selectFileOnly(item, items): Promise<FileTarget> {
   }
 
   // short cut
-  if (item === undefined || !item.fsPath) {
+  if (item === null || item === undefined || !item.fsPath) {
     return getActiveTarget();
   }
 
