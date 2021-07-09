a9b8769a4a79b3dbb782bbdbdb36a423440e0c76
Natizyskunk <natan.fourie@hotmail.fr>
2021-07-09 02:53:25 +0200

    commenting previous fix for the 'No such file' error on VSCode 1.56

--- src/core/fs/sftpFileSystem.ts
+++ src/core/fs/sftpFileSystem.ts
@@ -166,7 +166,9 @@ export default class SFTPFileSystem extends RemoteFileSystem {
 
   get(path, option?: FileOption): Promise<Readable> {
     return new Promise((resolve, reject) => {
+      // const opt = { ...option, autoDestroy: false };
       try {
+        // const stream = this.sftp.createReadStream(path, opt);
         const stream = this.sftp.createReadStream(path, option);
         resolve(stream);
       } catch (err) {
@@ -190,6 +192,7 @@ export default class SFTPFileSystem extends RemoteFileSystem {
   async put(input: Readable, path, option?: FileOption): Promise<void> {
     if (option && option.fd) {
       const fd = option.fd as SFTPFileDescriptor;
+      // const opt = { ...option, handle: fd.handle, autoDestroy: false };
       const opt = { ...option, handle: fd.handle };
       delete opt.fd;
 
